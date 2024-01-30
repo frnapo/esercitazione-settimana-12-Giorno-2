180e8cf21776af45e6d9f72743a82e15037c0e91
Francesco <napo.fra96@gmail.com>
2024-01-30 16:59:04 +0100

    esercitazione-settimana-12-Giorno-2

---
+++ es-settimana-12-Giorno-2/es-settimana-12-Giorno-2/Persona.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace es_settimana_12_Giorno_2
+{
+    internal class Persona
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public int Eta { get; set; }
+
+        public Persona(string Nome, string Cognome, int Eta)
+        {
+            this.Nome = Nome;
+            this.Cognome = Cognome;
+            this.Eta = Eta;
+        }
+
+        public void GetNome()
+        {
+            Console.WriteLine("Il nome della persona e': " + Nome);
+        }
+
+        public void GetCognome()
+        {
+            Console.WriteLine("Il cognome della persona e': " + Cognome);
+        }
+
+        public void GetEta()
+        {
+            Console.WriteLine("L'eta' della persona e': " + Eta + " anni.");
+        }
+
+        public string GetDettagli()
+        {
+            string messaggio = Nome + ". " + Cognome + ". " + Eta + " anni.";
+            return messaggio;
+        }
+
+    }
+}
---
+++ es-settimana-12-Giorno-2/es-settimana-12-Giorno-2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace es_settimana_12_Giorno_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona1 = new Persona("Francesco", "Napoli", 27);
+            persona1.GetNome();
+            persona1.GetCognome();
+            persona1.GetEta();
+            Console.WriteLine(persona1.GetDettagli());
+            Console.ReadLine();
+
+
+            Console.WriteLine("Inserisci nome persona:");
+            string Nome = Console.ReadLine();
+            Console.WriteLine("Inserisci cognome persona:");
+            string Cognome = Console.ReadLine();
+            Console.WriteLine("Inserisci eta' persona:");
+            int Eta = Convert.ToInt32(Console.ReadLine());
+            Persona persona2 = new Persona(Nome, Cognome, Eta);
+            Console.WriteLine(persona2.GetDettagli());
+            Console.ReadLine();
+        }
+    }
+}
